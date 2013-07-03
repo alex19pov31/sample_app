f3739d5e961a95d6f46050cc9e41451344963521
Нестеров Александр <alex19pov31@gmail.com>
2013-07-03 15:11:24 +0400

    Завершение 6 главы

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,106 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe User do
+  before { @user = User.new(name: "Example user", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
+  subject { @user }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should be_valid }
+  
+  describe "when name is not presence" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when name is to long" do
+    before { @user.name = "a"*51 }
+    it { should_not be_valid}
+  end
+  
+  describe "when email is not presence" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when email format" do
+    it "is invalid should be invalid" do
+      %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com].each do |adress|
+        @user.email = adress
+        should_not be_valid
+      end
+    end
+    
+    it "is valid sould be valid" do
+      %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn].each do |adress|
+        @user.email = adress
+        should be_valid
+      end
+    end
+  end
+  
+  #require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+  
+  describe "when email is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = user_with_same_email.email.upcase
+      user_with_same_email.save
+    end
+    
+    it { should_not be_valid }
+  end
+  
+  
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end  
+  
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+  
+  
+  it { should respond_to(:authenticate) }
+ 
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end 
+    
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+  
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+  
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+  
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end   
+end
